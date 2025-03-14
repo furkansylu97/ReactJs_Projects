a43da09500a548d5793389b5254461022e1610cf
Furkan Soylu <furkansylu97@gmail.com>
2025-03-14 16:11:25 +0300

    React project was created

---
+++ Courses/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      Project Start
+    </>
+  )
+}
+
+export default App
