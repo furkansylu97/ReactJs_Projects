2fc2f74444f5ab6b0ca397e96d956079466fe927
Furkan Soylu <furkansylu97@gmail.com>
2025-03-19 00:58:24 +0300

    Using usestate without using handleClick  method

--- UseState/src/App.jsx
+++ UseState/src/App.jsx
@@ -3,13 +3,12 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const handleClick = () => {
-    setCount (count + 1);
-  }
 
   return (
     <>
-      <button onClick={ handleClick }>Add Course</button>
+      <button onClick={ () => {
+        setCount (count + 1);
+      } }>Add Course</button>
       <div>Course Numbers: { count }</div>
     </>
   )
