93f7e51f3cf554fb1e7ba8b128ffa0042e0c6f63
Furkan Soylu <furkansylu97@gmail.com>
2025-03-25 22:44:12 +0300

    getRandomCourse function that will take the photo variable names we defined in the Course.jsx file as random string vals was defined

--- StateProject/src/App.jsx
+++ StateProject/src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import { Course } from './components/Course'
 
+function getRandomCourse() {
+  const courseArray = ['Angular','Bootstrap5','Allweb','Frontend'];
+  return courseArray[Math.floor(Math.random()*courseArray.length)]
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
