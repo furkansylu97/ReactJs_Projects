f7ecc62314af74bca870966922a175c60719aca5
Furkan Soylu <furkansylu97@gmail.com>
2025-03-25 22:27:53 +0300

    The function that will perform the defined operations when the button is clicked has been defined

--- StateProject/src/App.jsx
+++ StateProject/src/App.jsx
@@ -5,8 +5,13 @@ import { Course } from './components/Course'
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleClick = () => {
+    
+  }
+
   return (
     <>
+      <button onClick={handleClick}>Course Add</button>
       <Course />
     </>
   )
