ef7342eb3f3edd704932ae6d178785738c15133c
Furkan Soylu <furkansylu97@gmail.com>
2025-03-26 13:59:38 +0300

    Photos were printed on the screen according to the courseName props

--- StateProject/src/components/Course.jsx
+++ StateProject/src/components/Course.jsx
@@ -4,8 +4,15 @@ import Angular from '../assets/img/angular.jpg'
 import Bootstrap5 from '../assets/img/bootstrap5.png'
 import Frontend from '../assets/img/frontend.png'
 
-export const Course = () => {
+const courseMap = {
+  Angular : Angular ,
+  Bootstrap5 : Bootstrap5,
+  Frontend : Frontend,
+  Allweb : Allweb,
+}
+
+export const Course = ({ courseName }) => {
   return (
-    <img src={ Allweb } alt="" />
+    <img src={courseMap[courseName]} alt="" />
   )
 }
