9423dbe48626184af356f55e087f6d244c8eb64b
Furkan Soylu <furkansylu97@gmail.com>
2025-04-15 12:38:00 +0300

    Value we get from the form was sent the to the Api

--- ApiCall/src/components/SearchHeader.jsx
+++ ApiCall/src/components/SearchHeader.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const SearchHeader = ({ search }) => {
+
+    const [valueInput, setValue] = useState('')
     
     const handleFormSubmit = (event) => {
         event.preventDefault();
-        search('car');
+        search( valueInput );
+    }
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
     }
 
   return (
     <div className='searchwrapper'>
         <form onSubmit={ handleFormSubmit }>
             <label>What are you looking for?</label>
-            <input type="text" />
+            <input type="text" value={ valueInput } onChange={ handleChange } />
         </form>
     </div>
   )
