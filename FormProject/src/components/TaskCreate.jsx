90d13ccd0a35b67d6159c48c35560f51d906bdcb
Furkan Soylu <furkansylu97@gmail.com>
2025-04-16 00:25:45 +0300

    TaskCreate code architectuere was created

--- FormProject/src/components/TaskCreate.jsx
+++ FormProject/src/components/TaskCreate.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const TaskCreate = () => {
   return (
-    <div>TaskCreate</div>
+    <div>
+        <h3>Please Add Task!</h3>
+        <form>
+            <label>Title</label>
+            <input />
+            <label>Enter Your Task</label>
+            <textarea />
+            <button>Create</button>
+        </form>
+    </div>
   )
 }
