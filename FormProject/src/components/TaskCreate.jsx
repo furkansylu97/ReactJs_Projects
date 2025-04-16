706546487b83e0b03a6cc4d9ea140635fd881518
Furkan Soylu <furkansylu97@gmail.com>
2025-04-16 13:43:59 +0300

    The process of getting the value written to the title content has been done

--- FormProject/src/components/TaskCreate.jsx
+++ FormProject/src/components/TaskCreate.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const TaskCreate = () => {
+
+  const [title, setTitle] = useState('')
+
+  const handleTitleChange = (event) => {
+    setTitle( event.target.value );
+  }
+
   return (
     <div className='taskwrapper'>
         <h3>Please Add Task!</h3>
         <form className='formwrapper'>
             <label className='tasklabel'>Title</label>
-            <input className='taskinput' />
+            <input value={ title } onChange={ handleTitleChange } className='taskinput' />
             <label className='tasklabel'>Enter Your Task</label>
             <textarea className='taskinput' rows={5} />
             <button className='taskbtn'>Create</button>
