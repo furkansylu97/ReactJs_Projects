6ba27d14de735473988e0d57bc6382ddfd032d52
Furkan Soylu <furkansylu97@gmail.com>
2025-04-17 12:34:28 +0300

    Task values ​​were printed on the screen

--- FormProject/src/components/TaskShow.jsx
+++ FormProject/src/components/TaskShow.jsx
@@ -4,6 +4,11 @@ export const TaskShow = ({ task }) => {
     console.log(task);
     
   return (
-    <div>TaskShow</div>
+    <div>
+      <h3>Action To Be Taken</h3>
+      <p>{ task.title }</p>
+      <h3>Action To Be Taken Description</h3>
+      <p>{ task.taskDesc }</p>
+    </div>
   )
 }
