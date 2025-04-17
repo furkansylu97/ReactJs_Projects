ec035dc50511625dc91a74653d46775e378c5c2e
Furkan Soylu <furkansylu97@gmail.com>
2025-04-17 15:09:33 +0300

    Arrangements have been made regarding which form structure will be displayed when the Update button is pressed

--- FormProject/src/components/TaskShow.jsx
+++ FormProject/src/components/TaskShow.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
+import { TaskCreate } from './TaskCreate'
 
 export const TaskShow = ({ task, onDelete }) => {
+  const [showUpdate, setShowUpdate] = useState(false)
     
   const handleDeleteClick = () => {
     onDelete(task.id)
   }
 
+  const handleUpdateClick = () => {
+    setShowUpdate(!showUpdate);
+  }
+
   return (
     <div className='taskshowwrapper'>
-      <h3 className='taskshow-title'>Action To Be Taken</h3>
-      <p>{ task.title }</p>
-      <h3 className='taskshow-title'>Action To Be Taken Description</h3>
-      <p>{ task.taskDesc }</p>
-      <div>
-        <button className='taskshowdel-btn' onClick={ handleDeleteClick }>Delete</button>
-        <button className='taskshowupd-btn'>Update</button>
-      </div>
+      {showUpdate ? 
+      (<TaskCreate/>) 
+      :       
+      (<div>
+        <h3 className='taskshow-title'>Action To Be Taken</h3>
+        <p>{ task.title }</p>
+        <h3 className='taskshow-title'>Action To Be Taken Description</h3>
+        <p>{ task.taskDesc }</p>
+        <div>
+          <button className='taskshowdel-btn' onClick={ handleDeleteClick }>Delete</button>
+          <button className='taskshowupd-btn' onClick={ handleUpdateClick }>Update</button>
+        </div>
+      </div>)
+      }
     </div>
   )
 }
