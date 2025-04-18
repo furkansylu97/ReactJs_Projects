a9088b703f3c6f02c5bd16168123988edff47e75
Furkan Soylu <furkansylu97@gmail.com>
2025-04-18 07:17:14 +0300

    The functions and operations to be performed when the button is clicked were defined

--- UseEffectProject/src/App.jsx
+++ UseEffectProject/src/App.jsx
@@ -1,14 +1,16 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [furkan, setFurkan] = useState(0);
+  const [hattap, setHattap] = useState(0);
   return (
     <>
     <div className='App'>
-      <button>Increase Furkan's vote +1</button>
-      <div>Furkan vote value:</div>
-      <button>Increase Hattap's vote +1</button>
-      <div>Hattap vote value:</div>
+      <button onClick={() => setFurkan(furkan + 1)}>Increase Furkan's vote +1</button>
+      <div>Furkan vote value:  {furkan}</div>
+      <button onClick={() => setHattap(hattap + 1)}>Increase Hattap's vote +1</button>
+      <div>Hattap vote value: {hattap}</div>
     </div>
     </>
   )
