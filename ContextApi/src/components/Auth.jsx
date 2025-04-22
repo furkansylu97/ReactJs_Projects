fd7562fe3d03743f66106dce1db44a05f28d97c3
Furkan Soylu <furkansylu97@gmail.com>
2025-04-22 12:18:05 +0300

    Values from authContext were got

--- ContextApi/src/components/Auth.jsx
+++ ContextApi/src/components/Auth.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useContext } from 'react'
+import AuthContext from '../context/authContext'
 
 export const Auth = () => {
+    const{ status } = useContext( AuthContext );
+    console.log(status);
+
   return (
     <div>Auth</div>
   )
