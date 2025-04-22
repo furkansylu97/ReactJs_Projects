4568ba7103a0a3cc1ec119a5d84fa580f250abac
Furkan Soylu <furkansylu97@gmail.com>
2025-04-22 12:25:33 +0300

    Status value was printed to screen

--- ContextApi/src/components/Auth.jsx
+++ ContextApi/src/components/Auth.jsx
@@ -4,9 +4,13 @@ import AuthContext from '../context/authContext'
 
 export const Auth = () => {
     const{ status } = useContext( AuthContext );
-    console.log(status);
 
   return (
-    <div>Auth</div>
+    <div>
+        <h1>Did you log in ?</h1>
+        {
+            status ? <p>Yes, I have already logged in </p> : <p>No, I didn't log in !!</p>
+        }
+    </div>
   )
 }
