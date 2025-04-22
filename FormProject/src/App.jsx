3518c31731df7a349cdadc8c05c710f8c8c22f6f
Furkan Soylu <furkansylu97@gmail.com>
2025-04-22 14:21:13 +0300

    Methods moving operations were performed

--- FormProject/src/App.jsx
+++ FormProject/src/App.jsx
@@ -1,55 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import { TaskCreate } from './components/TaskCreate'
 import { Tasklist } from './components/Tasklist'
-import axios from 'axios'
+
 
 function App() {
-  const [tasks, setTasks] = useState([])
-
-  const createTask = async (title, taskDesc) => {
-    const response = await axios.post('http://localhost:3000/tasks',{
-      title,
-      taskDesc,
-    })
-    console.log(response);
-
-    const createdTasks = [
-      ...tasks, 
-      response.data
-    ];
-    setTasks(createdTasks);
-  }
-
-  const fetchTasks = async () => {
-    const response = await axios.get('http://localhost:3000/tasks');
-    setTasks(response.data);
-  }
 
   useEffect(() => {
     fetchTasks();
   },[])
-
-  const deleteTaskById = async (id)  => {
-    await axios.delete(`http://localhost:3000/tasks/${id}`);
-    const afterDeletingTasks = tasks.filter((tasks) => {
-      return tasks.id !== id;
-    })
-    setTasks(afterDeletingTasks);
-  }
-
-  const editTaskById = async (id, updatedTitle, updatedTaskDesc)  => {
-    await axios.put(`http://localhost:3000/tasks/${id}`, {
-      title:updatedTitle, taskDesc:updatedTaskDesc
-    });
-    const updatedTasks = tasks.map((tasks) => {
-      if (tasks.id === id) {
-        return {id,title:updatedTitle, taskDesc:updatedTaskDesc }   
-      }
-      return tasks;
-    })
-    setTasks(updatedTasks);
-  }
 
   return (
     <>
--- FormProject/src/context/Task.jsx
+++ FormProject/src/context/Task.jsx
@@ -1,9 +1,51 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import axios from 'axios'
 
 const TaskContext = createContext();
 
 function Provider ({ children }) {
 
+  const [tasks, setTasks] = useState([])
+
+  const createTask = async (title, taskDesc) => {
+    const response = await axios.post('http://localhost:3000/tasks',{
+      title,
+      taskDesc,
+    })
+    console.log(response);
+
+    const createdTasks = [
+      ...tasks, 
+      response.data
+    ];
+    setTasks(createdTasks);
+  }
+
+  const fetchTasks = async () => {
+    const response = await axios.get('http://localhost:3000/tasks');
+    setTasks(response.data);
+  }
+  
+  const deleteTaskById = async (id)  => {
+    await axios.delete(`http://localhost:3000/tasks/${id}`);
+    const afterDeletingTasks = tasks.filter((tasks) => {
+      return tasks.id !== id;
+    })
+    setTasks(afterDeletingTasks);
+  }
+
+  const editTaskById = async (id, updatedTitle, updatedTaskDesc)  => {
+    await axios.put(`http://localhost:3000/tasks/${id}`, {
+      title:updatedTitle, taskDesc:updatedTaskDesc
+    });
+    const updatedTasks = tasks.map((tasks) => {
+      if (tasks.id === id) {
+        return {id,title:updatedTitle, taskDesc:updatedTaskDesc }   
+      }
+      return tasks;
+    })
+    setTasks(updatedTasks);
+  }
 
     return (
         <TaskContext.Provider>
