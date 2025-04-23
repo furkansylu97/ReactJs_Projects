96af14bde0b6140fb6eb4a2f73bce308c5d93ed1
Furkan Soylu <furkansylu97@gmail.com>
2025-04-23 14:13:24 +0300

    Loading process was performed

--- CoursesProject/src/App.jsx
+++ CoursesProject/src/App.jsx
@@ -2,13 +2,23 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
 import { Courses } from './components/Courses'
+import { Loading } from './components/Loading';
 
 function App() {
-  const [courses, setCourses] = useState([])
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
 
   const fetchCourses = async () => {
-    const response = await axios.get('http://localhost:3000/courses');
-    setCourses(response.data);
+    setLoading(true);
+    try {
+      const response = await axios.get('http://localhost:3000/courses');
+      setCourses(response.data);
+      setLoading(false);
+      
+    } catch (error) {
+      setLoading(false);
+    }  
   }
 
   useEffect(() => {
@@ -19,7 +29,13 @@ function App() {
 
   return (
     <>
-      <Courses courses={ courses } />
+    <div className="App">
+      {loading ? (
+        <Loading />
+      ) : (
+          <Courses courses={ courses } />
+      )}
+    </div>
     </>
   )
 }
