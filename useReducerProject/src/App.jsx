f972954699c75c325f203282faf8dca12c2edfb5
Furkan Soylu <furkansylu97@gmail.com>
2025-04-24 11:56:29 +0300

    Errors were corrected

--- useReducerProject/src/App.jsx
+++ useReducerProject/src/App.jsx
@@ -6,9 +6,9 @@ function App() {
 
   const reducer = (state, action) => {
     switch(action){
-      case increment : return state +1;
-      case decrement : return state -1;
-      case reset : return initialValue;
+      case 'increment' : return state +1;
+      case 'decrement' : return state -1;
+      case 'reset' : return initialValue;
       default : return state;
     }
   }
