e29b5b73c3840a0334e6a5ae0137f477d1fcd453
Furkan Soylu <furkansylu97@gmail.com>
2025-04-24 11:44:18 +0300

    useReducer hooks was imported and Operations were defined

--- useReducerProject/src/App.jsx
+++ useReducerProject/src/App.jsx
@@ -1,8 +1,19 @@
+import { useReducer } from 'react'  
 import './App.css'
 
 function App() {
-  
+  const initialValue = 0;
 
+  const reducer = (state, action) => {
+    switch(action){
+      case increment : return state +1;
+      case decrement : return state -1;
+      case reset : return initialValue;
+      default : return state;
+    }
+  }
+  
+  const [counter, dispatch] = useReducer(reducer, initialValue )
   return (
     <>
       <div className="App">
