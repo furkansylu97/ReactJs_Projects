1b08a7e091c77fee779cec7f3c48aa79d6d8afa1
Furkan Soylu <furkansylu97@gmail.com>
2025-04-24 16:22:21 +0300

    useTransition project was created

---
+++ useTransition/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      useTransition Project
+    </>
+  )
+}
+
+export default App
