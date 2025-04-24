d7ed1f7183211d9219242669048e45c7e1c686c0
Furkan Soylu <furkansylu97@gmail.com>
2025-04-24 15:49:57 +0300

    Using useRef to give reference any Html element was performed

--- useRefProject/src/App.jsx
+++ useRefProject/src/App.jsx
@@ -7,18 +7,25 @@ function App() {
   const renderCount = useRef(0);
   // {current:0}
 
+  const inputRef = useRef();
+
   useEffect(() => {
   //  setRenderCount(( prev ) => prev + 1 ); //infinite loop
   renderCount.current = renderCount.current + 1;
   })
   
+  const focusInput = () => {
+    inputRef.current.focus();
+    inputRef.current.value = 'Furkan Soylu';
+  }
 
   return (
     <>
       <div className="App">
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input ref={inputRef} type="text" value={name} onChange={(e) => setName(e.target.value)} />
         <div>My name is {name}</div>
         <div>{renderCount.current} defa render oldu</div>
+        <button onClick={focusInput}>Focus</button>
     </div>
     </>
   )
