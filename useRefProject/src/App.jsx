c09ad10eee1756109e79ba8f9e67ce4e8b6f5dea
Furkan Soylu <furkansylu97@gmail.com>
2025-04-24 15:38:11 +0300

    useRef usage

--- useRefProject/src/App.jsx
+++ useRefProject/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
   const [name, setName] = useState('');
+  // const [renderCount, setRenderCount] = useState(second);
+  const renderCount = useRef(0);
+  // {current:0}
+
+  useEffect(() => {
+  //  setRenderCount(( prev ) => prev + 1 ); //infinite loop
+  renderCount.current = renderCount.current + 1;
+  })
+  
 
   return (
     <>
       <div className="App">
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
         <div>My name is {name}</div>
+        <div>{renderCount.current} defa render oldu</div>
     </div>
     </>
   )
