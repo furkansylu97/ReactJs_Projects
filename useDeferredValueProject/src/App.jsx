5c0bd884577e35e0559ccbc1b539ba0125c5c664
Furkan Soylu <furkansylu97@gmail.com>
2025-04-25 11:52:27 +0300

    Characters.jsx component contents were written

--- useDeferredValueProject/src/App.jsx
+++ useDeferredValueProject/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
      <input type="text" value={input} onChange={handleChange} />
-     <Characters />
+     <Characters input={input}/>
     </>
   )
 }
--- useDeferredValueProject/src/components/Characters.jsx
+++ useDeferredValueProject/src/components/Characters.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useMemo } from 'react'
 
-export const Characters = () => {
+export const Characters = ({ input }) => {
+
+    const myList = useMemo(() => {
+        const myArray = [];
+        for (let i = 0; i < 3000; i++) {
+            myArray.push(<div key={i}>{ input }</div>);     
+        }
+        return myArray;
+    }, [input]);
   return (
-    <div>Characters</div>
+    myList
   )
 }
