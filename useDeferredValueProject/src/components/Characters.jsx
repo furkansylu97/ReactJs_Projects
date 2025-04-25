ffdeeedd076993fd10f78e092146cc8dffd8c7a5
Furkan Soylu <furkansylu97@gmail.com>
2025-04-25 11:59:27 +0300

    useDeferredValue usage

--- useDeferredValueProject/src/components/Characters.jsx
+++ useDeferredValueProject/src/components/Characters.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { useMemo } from 'react'
+import { useMemo, useDeferredValue } from 'react'
 
 export const Characters = ({ input }) => {
+
+    const deferredValue = useDeferredValue(input);
 
     const myList = useMemo(() => {
         const myArray = [];
         for (let i = 0; i < 3000; i++) {
-            myArray.push(<div key={i}>{ input }</div>);     
+            myArray.push(<div key={i}>{ deferredValue }</div>);     
         }
         return myArray;
-    }, [input]);
+    }, [deferredValue]);
   return (
-    myList
+    <div>
+        {myList}
+    </div>
   )
 }
