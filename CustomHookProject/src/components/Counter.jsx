666f93d14e2bd903bc763e9304e149366fff99bc
Furkan Soylu <furkansylu97@gmail.com>
2025-04-27 23:34:01 +0300

    Buttons and actions to be performed when clicked were created

--- CustomHookProject/src/components/Counter.jsx
+++ CustomHookProject/src/components/Counter.jsx
@@ -5,9 +5,22 @@ export const Counter = () => {
 
     const [num, setNum] = useState(0)
 
+    const increment = () => {
+        setNum((prevNum) => prevNum + 1)
+    }
+    const reset = () => {
+        setNum(0)
+    }
+    const decrement = () => {
+        setNum((prevNum) => prevNum - 1)
+    }
+
   return (
     <div>
         <h2>Number = {num}</h2>
+        <button onClick={ increment }>Increase</button>
+        <button onClick={ reset }>Reset</button>
+        <button onClick={ decrement }>Decrease</button>
     </div>
   )
 }
