03edb8010c2bf3c9268e05a3054f464716c917a0
Furkan Soylu <furkansylu97@gmail.com>
2025-04-27 23:24:03 +0300

    Number was printed on the secren

--- CustomHookProject/src/components/Counter.jsx
+++ CustomHookProject/src/components/Counter.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const Counter = () => {
+
+    const [num, setNum] = useState(0)
+
   return (
-    <div>Counter</div>
+    <div>
+        <h2>Number = {num}</h2>
+    </div>
   )
 }
