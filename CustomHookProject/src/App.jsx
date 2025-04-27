c31c24ab6bfc345e2bb66cee68d5b7b253060fd5
Furkan Soylu <furkansylu97@gmail.com>
2025-04-27 23:20:40 +0300

    Counter component was created

--- CustomHookProject/src/App.jsx
+++ CustomHookProject/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import { TitleOne } from './components/TitleOne'
+import { Counter } from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <TitleOne />
+     <Counter />
     </>
   )
 }
