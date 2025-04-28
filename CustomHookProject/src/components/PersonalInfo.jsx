5f7d3de9a561a3c4315e41fea1a2456cf0609df7
Furkan Soylu <furkansylu97@gmail.com>
2025-04-28 14:07:11 +0300

    PersonalInfo component contents were written

--- CustomHookProject/src/components/PersonalInfo.jsx
+++ CustomHookProject/src/components/PersonalInfo.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 
 function PersonalInfo() {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
   return (
-    <div>PersonalInfo</div>
+    <div>
+        <form >
+            <div>
+                <label>Name</label>
+                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            </div>
+            <div>
+                <label>Lastname</label>
+                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+            </div>
+            <button>Save</button>
+        </form>
+    </div>
   )
 }
 
