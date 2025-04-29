b6d96d99269511cf71b8a5e5c275f645d63cfa5c
Furkan Soylu <furkansylu97@gmail.com>
2025-04-29 16:18:17 +0300

    Router integration was parformed

--- BurgerStoreProject/src/App.jsx
+++ BurgerStoreProject/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './components/Home'
+import { About } from './components/About'
+import { Contact } from './components/Contact'
+import { Menu } from './components/Menu'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      BurgerStoreProject
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/menu' element={<Menu />} />
+      </Routes>
     </>
   )
 }
