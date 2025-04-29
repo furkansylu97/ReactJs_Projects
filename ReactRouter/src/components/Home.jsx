9db0630128680b29940b85f81751aec880ef07d2
Furkan Soylu <furkansylu97@gmail.com>
2025-04-29 07:51:44 +0300

    When the button was clicked, a dynamic redirection was made from any page content to another page with useNavigate

--- ReactRouter/src/components/Home.jsx
+++ ReactRouter/src/components/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const Home = () => {
+
+    const navigate = useNavigate()
+
   return (
+    <>
     <div>Home</div>
+    <button onClick={() => navigate('/mission') }>Go to Mission</button>
+    </>
   )
 }
