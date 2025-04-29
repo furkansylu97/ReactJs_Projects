c417d545ca3735b4e1275e711730df09c7645e87
Furkan Soylu <furkansylu97@gmail.com>
2025-04-29 07:26:23 +0300

    Since the Navbar component will be on every page, it was defined at the top

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -3,15 +3,19 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './components/Home'
 import { AboutUs } from './components/AboutUs'
+import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar/>
+
      <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/aboutUs' element={<AboutUs/>} />
      </Routes>
+     
     </>
   )
 }
