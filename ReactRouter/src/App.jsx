26500619e14dd47cebaeb6aaea5a1b5bf6877006
Furkan Soylu <furkansylu97@gmail.com>
2025-04-29 09:19:38 +0300

    Dynamic Route works were performed

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -9,6 +9,8 @@ import { NotFoundPage } from './components/NotFoundPage'
 import { History } from './components/History'
 import { Company } from './components/Company'
 import { Team } from './components/Team'
+import { Members } from './components/Members'
+import { MemberDetail } from './components/MemberDetail'
 
 function App() {
 
@@ -24,6 +26,8 @@ function App() {
         <Route path='company' element={<Company/>} />
         <Route path='team' element={<Team/>} />
       </Route>
+      <Route path='/members' element={<Members/>} />
+      <Route path='/members/:memberId' element={<MemberDetail/>} />
       <Route path='*' element={<NotFoundPage/>} />
      </Routes>
 
---
+++ ReactRouter/src/components/MemberDetail.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MemberDetail = () => {
+  return (
+    <div>MemberDetail</div>
+  )
+}
