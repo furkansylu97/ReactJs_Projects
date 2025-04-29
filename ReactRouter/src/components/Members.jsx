40a449bae8104c776f9b573b5a0b9aabde67df7e
Furkan Soylu <furkansylu97@gmail.com>
2025-04-29 14:46:11 +0300

    A simple screen printing application was performed with filtering

--- ReactRouter/src/components/Members.jsx
+++ ReactRouter/src/components/Members.jsx
@@ -4,12 +4,14 @@ import { useSearchParams } from 'react-router-dom'
 export const Members = () => {
 
   const [searchParams, setSearchParams] = useSearchParams();
+  const isActive = searchParams.get('filter') === 'active';
 
   return (
     <>
       <div>Members</div>
       <button onClick={() => setSearchParams({filter : 'active'})}>Active Memebers</button>
       <button onClick={() => setSearchParams()}>Active Memebers</button>
+      {isActive ? <h2>Active Memebers</h2> : <h2>All Members</h2>}
     </>
   )
 }
