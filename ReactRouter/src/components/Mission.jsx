e96f33a0ebc53b2a8f09a7de690fa19ca4ab182a
Furkan Soylu <furkansylu97@gmail.com>
2025-04-29 07:58:08 +0300

    Page redirection was done with useNavigate

--- ReactRouter/src/components/Mission.jsx
+++ ReactRouter/src/components/Mission.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const Mission = () => {
+    
+    const navigate = useNavigate()
+    
   return (
+    <>
     <div>Mission</div>
+    <button onClick={() => navigate('/') }>Come Back</button>
+    </>
   )
 }
