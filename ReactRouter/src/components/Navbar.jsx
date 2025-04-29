8acd802cf1e8a44c35ad4c1218d3c47e2ec41930
Furkan Soylu <furkansylu97@gmail.com>
2025-04-29 07:36:33 +0300

    Active links were identified

--- ReactRouter/src/components/Navbar.jsx
+++ ReactRouter/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
     <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/aboutUs'>About</Link>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/aboutUs'>About</NavLink>
     </nav>
   )
 }
