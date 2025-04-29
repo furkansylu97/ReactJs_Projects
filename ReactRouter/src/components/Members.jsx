3c83bc6155b96fb1463c85c241ce2691c62030c6
Furkan Soylu <furkansylu97@gmail.com>
2025-04-29 14:35:47 +0300

    Active members were filtered with the useSearchParams method

--- ReactRouter/src/components/Members.jsx
+++ ReactRouter/src/components/Members.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 export const Members = () => {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
   return (
-    <div>Members</div>
+    <>
+      <div>Members</div>
+      <button onClick={() => setSearchParams({filter : 'active'})}>Active Memebers</button>
+    </>
   )
 }
