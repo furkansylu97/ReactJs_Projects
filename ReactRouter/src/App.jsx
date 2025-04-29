e21669858c43dca4c567b0d09d5a4bbd433b5b7f
Furkan Soylu <furkansylu97@gmail.com>
2025-04-29 07:03:02 +0300

    Home and AboutUs components were created and page redirects were performed

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './components/Home'
+import { AboutUs } from './components/AboutUs'
 
 function App() {
 
   return (
     <>
-     ReactRouter
+     <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/aboutUs' element={<AboutUs/>} />
+     </Routes>
     </>
   )
 }
