359ddf1dfbebb67c162b83b1fdf710867ebe256e
Furkan Soylu <furkansylu97@gmail.com>
2025-04-30 21:05:41 +0300

    Home.css file was created and css edits were performed

--- BurgerStoreProject/src/components/Home.jsx
+++ BurgerStoreProject/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import BannerNew from '../assets/img/bannernew.jpg'
+import '../styles/Home.css'
 
 export const Home = () => {
   return (
