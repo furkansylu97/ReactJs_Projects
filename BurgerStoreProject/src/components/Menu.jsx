5d821cbf1b630171bee71234fb62a9224474eccc
Furkan Soylu <furkansylu97@gmail.com>
2025-04-30 22:21:47 +0300

    Data was taken in the Menu.jsx file and the card structure was created in the menuItem.jsx file

--- BurgerStoreProject/src/components/Menu.jsx
+++ BurgerStoreProject/src/components/Menu.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Data } from '../helpers/Data'
+import { MenuItem } from './MenuItem'
 
 export const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className='menu'>
+      <h1 className='menuTitle'>Burgers</h1>
+      <div className="menuList">
+        {Data.map((menuItem, key) => {
+          return <MenuItem 
+            key={key}
+            image = {menuItem.image}
+            name = {menuItem.name}
+            content = {menuItem.content}
+            price = {menuItem.price}
+          />
+        })}
+      </div>
+    </div>
   )
 }
