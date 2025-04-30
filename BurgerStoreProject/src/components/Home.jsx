0e24c18041689491d78f0a940532b1644814d06b
Furkan Soylu <furkansylu97@gmail.com>
2025-04-30 20:57:55 +0300

    Home page contents were created

--- BurgerStoreProject/src/components/Home.jsx
+++ BurgerStoreProject/src/components/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import BannerNew from '../assets/img/bannernew.jpg'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='mainpage' style={{backgroundImage:`url(${BannerNew})`}}>
+      <div className='order'>
+        <Link to='/menu'>
+          <button>Order Now</button>
+        </Link>
+      </div>
+    </div>
   )
 }
