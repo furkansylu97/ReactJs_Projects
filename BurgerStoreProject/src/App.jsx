17d151016604b1d4bba547c9161c9cfba105bc4a
Furkan Soylu <furkansylu97@gmail.com>
2025-04-30 13:45:28 +0300

    Navbar.jsx file was created and logo was printed to screen

--- BurgerStoreProject/src/App.jsx
+++ BurgerStoreProject/src/App.jsx
@@ -4,11 +4,13 @@ import { Home } from './components/Home'
 import { About } from './components/About'
 import { Contact } from './components/Contact'
 import { Menu } from './components/Menu'
+import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar />
       <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
---
+++ BurgerStoreProject/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Burgerlogo from '../assets/img/burgerlogo.png'
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="main">
+            <img src={ Burgerlogo } alt="" />
+        </div>
+    </div>
+  )
+}
