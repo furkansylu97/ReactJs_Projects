841318c31457d02c11e7ae60e56b7577d84d3f79
Furkan Soylu <furkansylu97@gmail.com>
2025-04-30 22:02:43 +0300

    Data.jsx file and datas were created

---
+++ BurgerStoreProject/src/helpers/Data.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import BurgerImage from '../assets/img/burger.jpg'
+
+export const Data = [
+    {
+        name: 'Efso Burger',
+        image: BurgerImage,
+        content: '150g Butcher Burger, Potato, Pickle',
+        price: '250₺', 
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerImage,
+        content: '150g Butcher Burger, Potato, Pickle',
+        price: '250₺', 
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerImage,
+        content: '150g Butcher Burger, Potato, Pickle',
+        price: '250₺', 
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerImage,
+        content: '150g Butcher Burger, Potato, Pickle',
+        price: '250₺', 
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerImage,
+        content: '150g Butcher Burger, Potato, Pickle',
+        price: '250₺', 
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerImage,
+        content: '150g Butcher Burger, Potato, Pickle',
+        price: '250₺', 
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerImage,
+        content: '150g Butcher Burger, Potato, Pickle',
+        price: '250₺', 
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerImage,
+        content: '150g Butcher Burger, Potato, Pickle',
+        price: '250₺', 
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerImage,
+        content: '150g Butcher Burger, Potato, Pickle',
+        price: '250₺', 
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerImage,
+        content: '150g Butcher Burger, Potato, Pickle',
+        price: '250₺', 
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerImage,
+        content: '150g Butcher Burger, Potato, Pickle',
+        price: '250₺', 
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerImage,
+        content: '150g Butcher Burger, Potato, Pickle',
+        price: '250₺', 
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerImage,
+        content: '150g Butcher Burger, Potato, Pickle',
+        price: '250₺', 
+    },
+]
