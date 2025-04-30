2ea0886c1e3edd24d96cd08dddf2c916363a187a
Furkan Soylu <furkansylu97@gmail.com>
2025-04-30 22:50:55 +0300

    About page contents were performed

--- BurgerStoreProject/src/components/About.jsx
+++ BurgerStoreProject/src/components/About.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import BannerImage from '../assets/img/bannernew.jpg'
 
 export const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+      <div className="aboutTop" style={{backgroundImage: `url(${BannerImage})`}}></div>
+      <div className='aboutBottom'>
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, minima quam, distinctio maxime earum, praesentium beatae quia qui itaque cum nihil illo. Quisquam possimus sint porro accusamus iusto aspernatur temporibus, harum repellat non praesentium, perferendis consequatur ipsam, pariatur laudantium dolor provident! Animi, veniam asperiores! Impedit sequi amet, explicabo soluta cum minima esse! Quaerat omnis, hic harum quis error excepturi laudantium deleniti iure! Voluptate, adipisci itaque.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, minima quam, distinctio maxime earum, praesentium beatae quia qui itaque cum nihil illo. Quisquam possimus sint porro accusamus iusto aspernatur temporibus, harum repellat non praesentium, perferendis consequatur ipsam, pariatur laudantium dolor provident! Animi, veniam asperiores! Impedit sequi amet, explicabo soluta cum minima esse! Quaerat omnis, hic harum quis error excepturi laudantium deleniti iure! Voluptate, adipisci itaque.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, minima quam, distinctio maxime earum, praesentium beatae quia qui itaque cum nihil illo. Quisquam possimus sint porro accusamus iusto aspernatur temporibus, harum repellat non praesentium, perferendis consequatur ipsam, pariatur laudantium dolor provident! Animi, veniam asperiores! Impedit sequi amet, explicabo soluta cum minima esse! Quaerat omnis, hic harum quis error excepturi laudantium deleniti iure! Voluptate, adipisci itaque.</p>
+      </div>
+    </div>
   )
 }
