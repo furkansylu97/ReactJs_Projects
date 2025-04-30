3b5429f2558bb57e5b07c3b0a814b698e8946923
Furkan Soylu <furkansylu97@gmail.com>
2025-04-30 13:53:00 +0300

    Navbar page redirection operations were performed

--- BurgerStoreProject/src/components/Navbar.jsx
+++ BurgerStoreProject/src/components/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Burgerlogo from '../assets/img/burgerlogo.png'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="main">
             <img src={ Burgerlogo } alt="" />
+            <div>
+                <Link to='/'>Home</Link>
+                <Link to='/menu'>Menu</Link>
+                <Link to='/about'>About Us</Link>
+                <Link to='/contact'>Contact</Link>
+            </div>
         </div>
     </div>
   )
