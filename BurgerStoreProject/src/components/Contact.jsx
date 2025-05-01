3aa74729f6a77c6ef9389d4c4a101c5bd5f3ff21
Furkan Soylu <furkansylu97@gmail.com>
2025-05-01 10:48:56 +0300

    Contact.jsx file contents were performed

--- BurgerStoreProject/src/components/Contact.jsx
+++ BurgerStoreProject/src/components/Contact.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import Banner from '../assets/img/banner.png'
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <div className="leftSide" style={{backgroundImage: `url(${Banner})`}} >
+      </div>
+      <div className="rightSide">
+        <h1>Contact Us</h1>
+        <form>
+          <label>Name Surname</label>
+          <input type="text" name='name' placeholder='Please enter your name and surname...'/>
+          <label>E-mail</label>
+          <input type="email" name='email' placeholder='Please enter your e-mail adress...'/>
+          <label>Your Message</label>
+          <textarea rows={'6'} name='message' placeholder='Please enter your name and surname...'></textarea>
+        </form>
+      </div>
+    </div>
   )
 }
