c0c4759efabc50c2a9fa4345727614b1002bd2e9
Furkan Soylu <furkansylu97@gmail.com>
2025-05-01 12:42:21 +0300

    Routing operations were performed

---
+++ ReactFormik/src/components/GeneralForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const GeneralForm = () => {
+  return (
+    <div>GeneralForm</div>
+  )
+}
---
+++ ReactFormik/src/components/PortalForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PortalForm = () => {
+  return (
+    <div>PortalForm</div>
+  )
+}
