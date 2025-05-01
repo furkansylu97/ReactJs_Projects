7613f89e377d397ff78f1e5740cd48a05935cd50
Furkan Soylu <furkansylu97@gmail.com>
2025-05-01 13:23:48 +0300

    Form contents and integrations were completed

--- ReactFormik/src/components/GeneralForm.jsx
+++ ReactFormik/src/components/GeneralForm.jsx
@@ -6,6 +6,9 @@ export const GeneralForm = () => {
         const { values, errors, handleChange, handleSubmit} = useFormik({
           initialValues: {
             email: '',
+            age: '',
+            password: '',
+            confirmPassword: '',
           }
         });
 
@@ -14,9 +17,41 @@ export const GeneralForm = () => {
     <form>
         <div>
             <label>E-mail</label>
-            <input type="email" value={values.email} id='email' placeholder='Please enter your email adress...' />
+            <input 
+            type="email" 
+            value={values.email}
+            onChange={handleChange} 
+            id='email' 
+            placeholder='Please enter your email adress...' />
         </div>
-        
+        <div>
+            <label>Age</label>
+            <input 
+            type="number" 
+            value={values.age}
+            onChange={handleChange} 
+            id='age' 
+            placeholder='Please enter your age...' />
+        </div>
+        <div>
+            <label>Password</label>
+            <input 
+            type="password" 
+            value={values.password}
+            onChange={handleChange} 
+            id='password' 
+            placeholder='Please enter your password...' />
+        </div>
+        <div>
+            <label>Confirm Password</label>
+            <input 
+            type="password" 
+            value={values.confirmPassword}
+            onChange={handleChange} 
+            id='confirmPassword' 
+            placeholder='Please confirm your password...' />
+        </div> 
+        <button type='submit'>SAVE</button>     
     </form>
   )
 }
