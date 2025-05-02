fdef5706b40d1e4db1c8653102254631c3174596
Furkan Soylu <furkansylu97@gmail.com>
2025-05-02 11:14:59 +0300

    Recieve error messages process was performed

--- ReactFormik/src/components/CustomInput.jsx
+++ ReactFormik/src/components/CustomInput.jsx
@@ -1,10 +1,16 @@
+import { useField } from 'formik'
 import React from 'react'
 
 function CustomInput({label, ...props}) {
+
+    const [field, meta] = useField(props);
+
   return (
     <>
      <label>{label}</label>
-     <input type="text" />
+     <input {...field} {...props} 
+     className={meta.error ? 'input-error' : ''} />
+    {meta.error && <div className='error'>{meta.error}</div>}
     </>
   )
 }
